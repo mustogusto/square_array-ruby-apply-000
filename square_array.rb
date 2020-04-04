5b601f623f3cbfeba4d9dd9ef347d42e8291527c
Aisha S <a_shaheed@outlook.com>
2020-04-04 02:12:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  pace = []
+  array = [1, 2, 3]
+array.each { |n| pace.push(n ** 2) }
+pace
+end
+
+def square_array(array)
+  array = [9, 10, 16, 25]
+  pace = []
+ array.each {|n| pace.push(n ** 2)}
+ puts pace
+
 end
