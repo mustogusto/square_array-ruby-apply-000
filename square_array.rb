c841647aedea6643a2c3fc5aceacd80b1cdba887
Aisha S <a_shaheed@outlook.com>
2020-04-04 03:13:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
-  pace = []
-  array = [1, 2, 3]
-array.each { |n| pace.new << n ** 2 }
-pace
+    array = [1, 2, 3]
+    new_array = []
+array.each |n|
+  new_array.push(n ** 2)
+end
+  new_array
 end
